48aeb08459d28b0d9f7d8d02d96cd1b46ddceea5
Swissgears <sergius.barannik@gmail.com>
2019-07-18 20:11:23 +0300

    Hillel_Homework_2019

--- src/main/java/CheckPrime.java
+++ src/main/java/CheckPrime.java
@@ -1,25 +1,43 @@
+import java.util.Scanner;
+
 public class CheckPrime {
     public static void main(String[] args) {
 
+        System.out.println("Enter the number to check: ");
+        Scanner scanner = new Scanner(System.in);
+        int checkedNumber = scanner.nextInt();
 
-        int i, m = 0, flag = 0;
-        int n = 3;//it is the number to be checked
-        m = n / 2;
-        if (n == 0 || n == 1) {
-            System.out.println(n + " is not prime number");
+        int i, m, flag = 0;
+
+        m = checkedNumber / 2;
+
+        if (checkedNumber == 0 || checkedNumber == 1) {
+            System.out.println(checkedNumber + " is not prime number");
+
         } else {
+
             for (i = 2; i <= m; i++) {
-                if (n % i == 0) {
-                    System.out.println(n + " is not prime number");
+
+                if (checkedNumber % i == 0) {
+
+                    System.out.println(checkedNumber + " is not prime number");
                     flag = 1;
                     break;
                 }
             }
+
             if (flag == 0) {
-                System.out.println(n + " is prime number");
+                System.out.println(checkedNumber + " is prime number");
             }
         }//end of else
     }
 }
 
 
+
+
+
+
+/*
+Написать метод который проверяет число простое или нет. Если нет вывести все его делители.
+*/
