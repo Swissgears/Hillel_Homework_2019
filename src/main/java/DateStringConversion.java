04d167d508ed86b815033770ac24bf564033fac3
Swissgears <sergius.barannik@gmail.com>
2019-07-25 20:46:16 +0300

    Преобразование строки с датой и временем

--- src/main/java/DateStringConversion.java
+++ src/main/java/DateStringConversion.java
@@ -8,10 +8,11 @@ public class DateStringConversion {
 
         System.out.println("Enter the date and time in the format 22.01.2019 19:15:00");
 
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine().trim();
-        System.out.println(dateFormatting(line));
-
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine().trim();
+            System.out.println(dateFormatting(line));
+        }
     }
 
     private static String dateFormatting(String inputString) {
@@ -30,6 +31,7 @@ public class DateStringConversion {
     }
 
 }
+
 /*
  Задана строка с датой вида "22.01.2019 19:15:00". Написать метод который преобразовывает ее в формат "22 Jan, 7:15 PM". Предусмотреть обработку ситуации если входящая дата в недопустимом формате.
  */
