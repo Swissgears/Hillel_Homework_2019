b40bb384fe623c45a794af6894d18d06ba248c74
Swissgears <sergius.barannik@gmail.com>
2019-09-25 22:02:19 +0300

    Написать класс MyApiService с использованием паттерна синглтон для доступа к API:
    (предварительно создать все необходимые модели List, Post, User...)

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,18 +1,87 @@
-public class User {
 
-    private String name;
-    private int age;
+    public class User {
+        private Integer id;
+        private String name;
+        private String username;
+        private String email;
+        private Address address;
+        private String phone;
+        private String website;
+        private Company company;
 
-    public User(String name, int age) {
-        this.name = name;
-        this.age = age;
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Address getAddress() {
+            return address;
+        }
+
+        public void setAddress(Address address) {
+            this.address = address;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        public String getWebsite() {
+            return website;
+        }
+
+        public void setWebsite(String website) {
+            this.website = website;
+        }
+
+        public Company getCompany() {
+            return company;
+        }
+
+        public void setCompany(Company company) {
+            this.company = company;
+        }
+
+        @Override
+        public String toString() {
+            return "id = " + id + "\n" +
+                    "name = " + name + "\n" +
+                    "username = " + username + "\n" +
+                    "email = " + email + "\n" +
+                    "address = " + address + "\n" +
+                    "phone = " + phone + "\n" +
+                    "website = " + website + "\n" +
+                    "company = " + company + "\n" + "\n";
+        }
     }
-    public String SayName() {
-       return "Hello! My name is " + name + "!";
-    }
-
-    public int SayAge() {
-      return age;
-    }
-}
-
